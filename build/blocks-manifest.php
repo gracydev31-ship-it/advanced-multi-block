9bb2757213cb8524a23b7dce95fe0697816b448f
gracydev31-ship-it <gracydev31@gmail.com>
2026-05-16 20:00:38 +0530

    news starting

--- build/blocks-manifest.php
+++ build/blocks-manifest.php
@@ -22,28 +22,6 @@ return array(
 		'style' => 'file:./style-index.css',
 		'render' => 'file:./render.php'
 	),
-	'banner' => array(
-		'$schema' => 'https://schemas.wp.org/trunk/block.json',
-		'apiVersion' => 3,
-		'name' => 'create-block/banner',
-		'version' => '0.1.0',
-		'title' => 'Banner',
-		'category' => 'widgets',
-		'icon' => 'smiley',
-		'description' => 'Example block scaffolded with Create Block tool.',
-		'example' => array(
-			
-		),
-		'supports' => array(
-			'html' => false
-		),
-		'textdomain' => 'advanced-multi-block',
-		'editorScript' => 'file:./index.js',
-		'editorStyle' => 'file:./index.css',
-		'style' => 'file:./style-index.css',
-		'render' => 'file:./render.php',
-		'viewScript' => 'file:./view.js'
-	),
 	'coach-athletes' => array(
 		'$schema' => 'https://schemas.wp.org/trunk/block.json',
 		'apiVersion' => 3,
@@ -302,49 +280,6 @@ return array(
 			'html' => false
 		),
 		'textdomain' => 'runpartner',
-		'editorScript' => 'file:./index.js',
-		'editorStyle' => 'file:./index.css',
-		'style' => 'file:./style-index.css',
-		'render' => 'file:./render.php',
-		'viewScriptModule' => 'file:./view.js'
-	),
-	'slider' => array(
-		'$schema' => 'https://schemas.wp.org/trunk/block.json',
-		'apiVersion' => 3,
-		'name' => 'create-block/slider',
-		'version' => '0.1.0',
-		'title' => 'Slider',
-		'category' => 'widgets',
-		'icon' => 'smiley',
-		'description' => 'Example block scaffolded with Create Block tool.',
-		'example' => array(
-			
-		),
-		'supports' => array(
-			'html' => false
-		),
-		'textdomain' => 'advanced-multi-block',
-		'editorScript' => 'file:./index.js',
-		'editorStyle' => 'file:./index.css',
-		'style' => 'file:./style-index.css',
-		'viewScript' => 'file:./view.js'
-	),
-	'toggle' => array(
-		'$schema' => 'https://schemas.wp.org/trunk/block.json',
-		'apiVersion' => 3,
-		'name' => 'create-block/toggle',
-		'version' => '0.1.0',
-		'title' => 'Toggle',
-		'category' => 'widgets',
-		'icon' => 'media-interactive',
-		'description' => 'An interactive block with the Interactivity API.',
-		'example' => array(
-			
-		),
-		'supports' => array(
-			'interactivity' => true
-		),
-		'textdomain' => 'advanced-multi-block',
 		'editorScript' => 'file:./index.js',
 		'editorStyle' => 'file:./index.css',
 		'style' => 'file:./style-index.css',
